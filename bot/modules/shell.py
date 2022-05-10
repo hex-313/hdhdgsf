4ad3344bafb8a7270cea7d3718d0936e21feebd3
hex-313 <78981792+hex-313@users.noreply.github.com>
2022-05-10 19:19:01 +0530

    Update shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -57,5 +57,5 @@ def shell(update: Update, context: CallbackContext):
         message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
 
-SHELL_HANDLER = CommandHandler(['sh', 'b', 'shell', 'run'], shell)
+SHELL_HANDLER = CommandHandler(['jb', 'shell'], shell)
 dispatcher.add_handler(SHELL_HANDLER)
